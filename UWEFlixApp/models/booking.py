5748dc2d1075a228147d7d974f0e371ea4e275ba
Joshua Saxby <joshua.a.saxby@gmail.com>
2023-02-21 20:17:47 +0000

    Modify setup of foreign keys to use model names rather than classes We could probably just import the classes here and refer to them directly rather than by string name, however if we later want circular relationships, we will not be able to keep doing that due to circular imports. I say we stick with referring them by name instead to keep things simple.

--- UWEFlixApp/models/booking.py
+++ UWEFlixApp/models/booking.py
@@ -6,7 +6,7 @@ class Booking(models.Model):
     bookingID = models.AutoField(primary_key=True)
     # to be added once authentication is completed
     #userID = models.ForeignKey(Showing, on_delete=models.CASCADE)
-    screeningID = models.ForeignKey(Screening, on_delete=models.CASCADE)
+    screeningID = models.ForeignKey('Screening', on_delete=models.CASCADE)
     numTickets = models.IntegerField()
     numTotal = models.IntegerField()
     def __str__(self):
--- UWEFlixApp/models/screening.py
+++ UWEFlixApp/models/screening.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Screening(models.Model):
     screeningID = models.AutoField(primary_key=True)
-    movieID = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    screenID = models.ForeignKey(Screen, on_delete=models.CASCADE)
+    movieID = models.ForeignKey('Movie', on_delete=models.CASCADE)
+    screenID = models.ForeignKey('Screen', on_delete=models.CASCADE)
     showingTime = models.TimeField(auto_now=False, auto_now_add=False)
     showingDate = models.DateField()
     remainingSeats = models.IntegerField()
