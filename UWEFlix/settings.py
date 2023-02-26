4d560cf531c2c22730fbf27efe064eca3fdc7198
Joshua Saxby <joshua.a.saxby@gmail.com>
2023-02-26 20:31:22 +0000

    Cleanup: Remove accidentally stored .pyc files Also modified config to use sqlite temporarily for me to test on my machine

--- UWEFlix/settings.py
+++ UWEFlix/settings.py
@@ -85,11 +85,10 @@ WSGI_APPLICATION = 'UWEFlix.wsgi.application'
 #         'PORT': '3306',  
 #     }
 # }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'daesd',
     }
 }
 
