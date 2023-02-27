53733a0947aa922d129fab262b930f7380f94f04
Joshua Saxby <joshua.a.saxby@gmail.com>
2023-02-27 18:26:11 +0000

    Added a role field to the User model, with roles for: - Cinema Manager (M) - Accounts Manager (A) - Club Rep (R) - Customer (C)

--- UWEFlixApp/models/user.py
+++ UWEFlixApp/models/user.py
@@ -1,4 +1,18 @@
+from enum import Enum
+
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
 
 class User(AbstractUser):
-    pass
+    class Role(Enum):
+        CINEMA_MANAGER = 'M'
+        ACCOUNT_MANAGER = 'A'
+        CLUB_REP = 'R'
+        CUSTOMER = 'C'
+
+    role = models.CharField(
+        max_length=1,
+        choices=[(tag, tag.value) for tag in Role]  # unpack Enum choices
+    )
