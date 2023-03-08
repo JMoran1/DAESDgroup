c0bea6fc30af59f381a1fad2e8a126aebb2f6200
JMoran1 <45739562+JMoran1@users.noreply.github.com>
2023-03-08 14:23:55 +0000

    Added tests for new funcs and pages created

--- UWEFlixApp/tests.py
+++ UWEFlixApp/tests.py
@@ -49,3 +49,83 @@ class CreateClubTests(TestCase):
         self.assertEqual(self.post.card_expiry, "2020-12-31")
         self.assertEqual(self.post.discount_rate, 0.1)
         self.assertEqual(self.post.address, "Bristol")
+
+class AccountManagerTest(TestCase):
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/account_manager')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse("account_manager"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse("account_manager"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "UWEFlixApp/account_manager_page.html")
+
+class CreateMonthlyStatementTest(TestCase):
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/create_monthly_statement')
+        self.assertEqual(response.status_code, 301)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse("create_monthly_statement"))
+        self.assertEqual(response.status_code, 302)
+
+class ViewClubsTest(TestCase):
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/view_clubs/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse("view_clubs"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse("view_clubs"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "UWEFlixApp/view_clubs.html")
+
+class UpdateClubTest(TestCase):
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/update_club/1/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse("update_club", args=[1]))
+        self.assertEqual(response.status_code, 200)
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Club.objects.create(name="UWEFlix", card_number="123456", card_expiry="2020-12-31", discount_rate=0.1, address="Bristol")
+    
+    def test_model_content(self):
+        self.assertEqual(self.post.name, "UWEFlix")
+        self.assertEqual(self.post.card_number, "123456")
+        self.assertEqual(self.post.card_expiry, "2020-12-31")
+        self.assertEqual(self.post.discount_rate, 0.1)
+        self.assertEqual(self.post.address, "Bristol")
+
+class DeleteClubTest(TestCase):
+    
+        def test_view_url_exists_at_desired_location(self):
+            response = self.client.get('/delete_club/1/')
+            self.assertEqual(response.status_code, 302)
+    
+        def test_view_url_accessible_by_name(self):
+            response = self.client.get(reverse("delete_club", args=[1]))
+            self.assertEqual(response.status_code, 302)
+        
+        @classmethod
+        def setUpTestData(cls):
+            cls.post = Club.objects.create(name="UWEFlix", card_number="123456", card_expiry="2020-12-31", discount_rate=0.1, address="Bristol")
+        
+        def test_model_content(self):
+            self.assertEqual(self.post.name, "UWEFlix")
+            self.assertEqual(self.post.card_number, "123456")
+            self.assertEqual(self.post.card_expiry, "2020-12-31")
+            self.assertEqual(self.post.discount_rate, 0.1)
+            self.assertEqual(self.post.address, "Bristol")
