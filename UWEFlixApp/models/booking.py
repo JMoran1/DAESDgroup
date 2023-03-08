5d3055d241793af0b929477c56dfd5006fad79ab
JMoran1 <45739562+JMoran1@users.noreply.github.com>
2023-03-08 12:50:01 +0000

    Added total price and club to the booking model

--- UWEFlixApp/models/booking.py
+++ UWEFlixApp/models/booking.py
@@ -6,6 +6,8 @@ class Booking(models.Model):
     # user = models.ForeignKey(User, on_delete=models.CASCADE)
     screening = models.ForeignKey('Screening', on_delete=models.CASCADE)
     number_of_tickets = models.IntegerField()
+    total_price = models.DecimalField(max_digits=6, decimal_places=2)
+    club = models.ForeignKey('Club', on_delete=models.CASCADE, null=True, blank=True)
  
     def __str__(self):
         return str(self.id)
