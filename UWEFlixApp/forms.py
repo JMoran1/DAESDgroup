d07117ecfefc5a1c86a7992eebd9148bab97c871
JMoran1 <45739562+JMoran1@users.noreply.github.com>
2023-03-15 11:49:16 +0000

    Update UWEFlixApp/forms.py
    changed label from password2
    
    Co-authored-by: Joshua Saxby <joshua.a.saxby+UMvLnvbsOxBHaeiCHvbdunpz@gmail.com>

--- UWEFlixApp/forms.py
+++ UWEFlixApp/forms.py
@@ -102,7 +102,7 @@ class ClubTopUpForm(forms.Form):
 class CustomerRegistrationForm(forms.Form):
     username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}), label='Password')
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}), label='Confirm Password')
 
     def clean_password2(self):
         password1 = self.cleaned_data['password1']
