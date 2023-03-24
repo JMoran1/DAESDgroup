9e396b3733db9641cba882d3b927e4f671963288
Joshua Saxby <joshua.a.saxby@gmail.com>
2023-03-24 14:56:26 +0000

    Some mucking about trying to work on #25

--- UWEFlixApp/models/screen.py
+++ UWEFlixApp/models/screen.py
@@ -1,10 +1,13 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
 class Screen(models.Model):
     name = models.CharField(max_length=25)
     description = models.TextField(null=False, blank=True)
-    capacity = models.IntegerField()
+    capacity = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(limit_value=1)]
+    )
 
     def __str__(self):
         return self.name
--- UWEFlixApp/models/screening.py
+++ UWEFlixApp/models/screening.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Count
 from UWEFlixApp.models import Movie, Screen
 
 
@@ -6,12 +7,30 @@ class Screening(models.Model):
     movie = models.ForeignKey('Movie', on_delete=models.CASCADE)
     screen = models.ForeignKey('Screen', on_delete=models.CASCADE)
     showing_at = models.DateTimeField(auto_now=False, auto_now_add=False)
-    '''
-    TODO: it might be better to instead create a method which returns the number
-    of seats remaining by querying the total number of seats for the screen and
-    subtracting the total number of places across all bookings for the screening
-    '''
-    seats_remaining = models.IntegerField(default=0)
+    
+    @classmethod
+    def with_seats_remaining(cls):
+        """
+        Returns new QuerySet with all Screening objects, annotated with a
+        subquery giving the number of remaining bookable seats per Screening
+        """
+        pass
+
+    @classmethod
+    def bookable(cls):
+        """
+        Returns a new QuerySet of all bookable Screenings (those that have some
+        seats remaining and which didn't start in the past)
+        """
+        pass
+
+    @property
+    def seats_remaining(self):
+        return self.booking_set.annotate(seats_allocated=Count('number_of_tickets'))
+
+    @property
+    def is_bookable(self):
+        return self.seats_remaining > 0
 
     def __str__(self):
         return f"{self.movie.name} - {self.screen.name} - {self.showing_at}"
