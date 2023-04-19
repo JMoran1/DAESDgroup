12522614719ab056e6ebfb63128fcec65979ce43
JMoran1 <45739562+JMoran1@users.noreply.github.com>
2023-04-19 21:27:06 +0100

    Fixed bug that breaks page when image is cleared

--- UWEFlixApp/models/movie.py
+++ UWEFlixApp/models/movie.py
@@ -1,5 +1,8 @@
 from django.core.validators import MinValueValidator
 from django.db import models
+from django.conf import settings
+import os
+from django.core.files import File
 
 
 class Movie(models.Model):
@@ -13,3 +16,12 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+        # Check if the image field is empty
+        if not self.image:
+            # Set the image field back to its default value
+            default_image_path = os.path.join(settings.MEDIA_ROOT, 'images', 'no_image_available.png')
+            self.image.save('no_image_available.png', File(open(default_image_path, 'rb')))
+
+        super(Movie, self).save(*args, **kwargs)
