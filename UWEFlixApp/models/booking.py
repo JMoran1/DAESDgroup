c243fc79fcff5d765c4a8be1a1c6c9127bc7624f
TylerIves94 <tylerjayives@msn.com>
2023-04-21 14:55:56 +0100

    modified the booking statuses as requested by josh and removed migrations

--- UWEFlixApp/models/booking.py
+++ UWEFlixApp/models/booking.py
@@ -1,8 +1,14 @@
 from django.db import models
 
+class Status(models.TextChoices):
+    Active = 'A', 'Active'
+    Cancel_Requested = 'CR', 'Cancel_Requested'
+    CLUB_REP = 'C', 'Cancelled'
+
 
 class Booking(models.Model):
     # to be added once authentication is completed
+    Status = Status
     user = models.ForeignKey('User', on_delete=models.CASCADE, null=True, blank=True)
     screening = models.ForeignKey('Screening', on_delete=models.CASCADE)
     number_of_adult_tickets = models.IntegerField()
@@ -11,6 +17,6 @@ class Booking(models.Model):
     total_price = models.DecimalField(max_digits=6, decimal_places=2, null=True)
     club = models.ForeignKey('Club', on_delete=models.CASCADE, null=True, blank=True)
     date = models.DateTimeField(auto_now=True, blank=True, null=True, max_length=100)
-    cancel_requested = models.TextField(default='Active')
+    cancel_requested = models.CharField(max_length=3, choices = Status.choices, default='Active')
     def __str__(self):
         return str(self.user)
