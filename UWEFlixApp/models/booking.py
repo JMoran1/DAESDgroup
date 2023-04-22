10e769730b4215bf86e6f27c3667b86d0210aa65
TylerIves94 <tylerjayives@msn.com>
2023-04-22 22:14:01 +0100

    Carrying out most requested changes to close branch

--- UWEFlixApp/models/booking.py
+++ UWEFlixApp/models/booking.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Status(models.TextChoices):
-    ACTIVE = ('A', 'Active',)
-    CANCELLATION_REQUESTED = ('R', 'Cancellation Requested',)
-    CANCELLED = ('C', 'Cancelled',)
+    ACTIVE = ('Active')
+    CANCELLATION_REQUESTED = ('Cancellation Requested')
+    CANCELLED = ('Cancelled')
 
 
 class Booking(models.Model):
@@ -17,6 +17,6 @@ class Booking(models.Model):
     total_price = models.DecimalField(max_digits=6, decimal_places=2, null=True)
     club = models.ForeignKey('Club', on_delete=models.CASCADE, null=True, blank=True)
     date = models.DateTimeField(auto_now=True, blank=True, null=True, max_length=100)
-    status = models.CharField(max_length=1, choices=Status.choices, default=Status.ACTIVE)
+    status = models.CharField(max_length=25, choices=Status.choices, default=Status.ACTIVE)
     def __str__(self):
         return str(self.user)
