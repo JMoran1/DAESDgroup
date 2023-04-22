377c0456a63acbe70bf87d4cd720d1ac8431efe3
Joshua Saxby <joshua.a.saxby@gmail.com>
2023-04-22 17:47:22 +0100

    Wrote a fun queryset-wrapper that computes seats remaining in the db!

--- UWEFlixApp/models/screening.py
+++ UWEFlixApp/models/screening.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import F, Sum
 from UWEFlixApp.models import Movie, Screen
 
 
@@ -6,12 +7,26 @@ class Screening(models.Model):
     movie = models.ForeignKey('Movie', on_delete=models.CASCADE)
     screen = models.ForeignKey('Screen', on_delete=models.CASCADE)
     showing_at = models.DateTimeField(auto_now=False, auto_now_add=False)
-    '''
-    TODO: it might be better to instead create a method which returns the number
-    of seats remaining by querying the total number of seats for the screen and
-    subtracting the total number of places across all bookings for the screening
-    '''
-    seats_remaining = models.IntegerField(default=0)
+
+    @property
+    def seats_remaining(self):
+        """
+        Reuse existing queryset wrapper.
+        
+        A bit awkward but less code duplication.
+        """
+        return Screening.objects_with_seats_remaining(
+        ).filter(id=self.id).values_list('_seats_remaining')[0][0]
 
     def __str__(self):
          return f"{self.movie} - {self.showing_at}"
+
+    @classmethod
+    def objects_with_seats_remaining(cls):
+        return cls.objects.annotate(  # first sum up all allocated ticket types
+            adult_tickets=Sum('booking__number_of_adult_tickets'),
+            child_tickets=Sum('booking__number_of_child_tickets'),
+            student_tickets=Sum('booking__number_of_student_tickets')
+        ).annotate(  # then subtract them from Screen capacity
+            _seats_remaining=F('screen__capacity') - (F('adult_tickets') + F('child_tickets') + F('student_tickets'))
+        )
