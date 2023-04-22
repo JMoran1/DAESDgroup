7bc5f3bafadc4a6df68a937b41e1301400333584
Joshua Saxby <joshua.a.saxby@gmail.com>
2023-04-22 17:18:30 +0100

    Set all eligible decimal fields to be non-negative where this applies

--- UWEFlixApp/models/booking.py
+++ UWEFlixApp/models/booking.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -8,7 +9,12 @@ class Booking(models.Model):
     number_of_adult_tickets = models.IntegerField()
     number_of_child_tickets = models.IntegerField()
     number_of_student_tickets = models.IntegerField()
-    total_price = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    total_price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=True,
+        validators=[MinValueValidator(limit_value=0)]  # can't be negative
+    )
     club = models.ForeignKey('Club', on_delete=models.CASCADE, null=True, blank=True)
     date = models.DateTimeField(auto_now=True, blank=True, null=True, max_length=100)
  
