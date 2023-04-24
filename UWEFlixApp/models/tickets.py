5f671e2bed07b3204273598be2c257d462916417
Joshua Saxby <joshua.a.saxby@gmail.com>
2023-04-24 18:02:46 +0100

    Initial work changifying tickets Moving to slug-based UID implementation

--- UWEFlixApp/models/tickets.py
+++ UWEFlixApp/models/tickets.py
@@ -2,7 +2,8 @@ from django.core.validators import MinValueValidator
 from django.db import models
 
 class Ticket(models.Model):
-    ticket_type = models.CharField(max_length=50)
+    uid = models.SlugField(max_length=50, unique=True)
+    type = models.CharField(max_length=50, unique=True)
     price = models.DecimalField(
         max_digits=5, decimal_places=2,
         validators=[MinValueValidator(limit_value=0)] # price can't be negative
