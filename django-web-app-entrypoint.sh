5877842cf93d3cb1af7a48677366df2164fe03b9
JMoran1 <45739562+JMoran1@users.noreply.github.com>
2023-05-08 15:52:33 +0100

    Added basic data for import + docker code

--- django-web-app-entrypoint.sh
+++ django-web-app-entrypoint.sh
@@ -4,5 +4,12 @@ echo "Waiting for MySQL Database Service to start..."
 
 ./wait-for mysql-db:3306
 
+set -e
 python manage.py migrate
+
+# Load data into database
+if [ ! -f /data_loaded.txt ]; then
+    python manage.py loaddata data.json
+    touch /data_loaded.txt
+fi
 python manage.py runserver 0.0.0.0:8000
